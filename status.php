d269841687f9b1879f8f872a688ee41143284ae5
Akmal Aliffandhi Anwar <90572183+akmalaa@users.noreply.github.com>
2023-03-17 23:31:49 +0700

    add button "+"

--- status.php
+++ status.php
@@ -29,6 +29,17 @@
         </div> -->
         <div class="tabel">
             <table style="width: 90%;">
+            <div class="kontol">
+                <a class="link" href="./add_agendaruangan.php">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="addel" viewBox="0 0 24 24"><path fill="black" d="M11 19v-6H5v-2h6V5h2v6h6v2h-6v6Z"/></svg>
+
+                </a>
+                <div class="hoveredText">
+                    <p>Tambah Agenda</p>
+                    <span>&#129305;</span>
+                </div>
+            </div>
+
                 <tr>
                     <th>No</th>
                     <th>Nama</th>
