474d71de3921292848f95bd86d8da2252382223f
Akmal Aliffandhi Anwar <90572183+akmalaa@users.noreply.github.com>
2023-03-17 21:20:42 +0700

    Foldering sidebar

--- konf_agendaruangan.php
+++ konf_agendaruangan.php
@@ -16,42 +16,9 @@
 
 <body>
     <section class="sidebarr">
-        <div class="sidebar">
-            <img class="logo" src="./assets/cropped-Logo-BPK-crop.png" alt="Logo BPK">
-            <ul>
-                <a href="./index.php">
-                    <li>Dashboard</li>
-                </a>
-                <a href="./konf_agendaruangan.php">
-                    <li>Konfirmasi Peminjaman</li>
-                </a>
-                <a href="./add_agendaruangan.php">
-                    <li>Form Peminjaman</li>
-                </a>
-                <a href="./status.php">
-                    <li>Status Ajuan</li>
-                </a>
-                <a href="./add_ruangan.php">
-                    <li>Tambah Ruangan</li>
-                </a>
-            </ul>
-        </div>
-
-        <div class="sidebarDum">
-            <img class="logo" src="./assets/cropped-Logo-BPK-crop.png" alt="Logo BPK">
-            <ul>
-                <a href="./konf_agendaruangan.php">
-                    <li>Konfirmasi Peminjaman</li>
-                </a>
-                <a href="./index.php">
-                    <li>Form Peminjaman</li>
-                </a>
-                <a href="./status.php">
-                    <li>Status Ajuan</li>
-                </a>
-                
-            </ul>
-        </div>
+        <?php
+            include ('./sidebar/sidebar.php')
+        ?>
     </section>
 
     <section class="main">
